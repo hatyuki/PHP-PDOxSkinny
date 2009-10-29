18d5edef2bd1902b19525476e126a593fa361cd4
magicalhat <yuki@apps.magicalhat.jp>
2009-10-29 17:58:12 +0900

    Profiler つくってみたよ

---
+++ lib/Skinny/Profiler.php
@@ -0,0 +1,51 @@
+<?php  // vim: ts=4 sts=4 sw=4
+
+
+// SkinnyProfiler based on DBIx::Skinny 0.04
+class SkinnyProfiler
+{
+    public $query_log = array( );
+
+
+    function __construct ( )
+    {
+        $this->reset( );
+    }
+
+
+    function reset ( )
+    {
+        $this->query_log = array( );
+    }
+
+
+    function record_query ($sql, $bind=array( ))
+    {
+        $log = $this->normalize($sql);
+
+        if ( sizeof($bind) != 0) {
+            $log .= ' :binds '.join(', ', array_map(
+                array($this, 'join_bind'), $bind)
+            );
+        }
+
+        $this->query_log[ ] = $log;
+    }
+
+
+    private function normalize ($sql)
+    {
+        $sql = preg_replace('/^\s*/',    '', $sql);
+        $sql = preg_replace('/\s*$/',    '', $sql);
+        $sql = preg_replace('/[\r\n]/', ' ', $sql);
+        $sql = preg_replace('/\s+/',    ' ', $sql);
+
+        return $sql;
+    }
+
+
+    private function join_bind ($b)
+    {
+        return is_null($b) ? 'null': $b; 
+    }
+}
---
+++ t/202_profiler.php
@@ -0,0 +1,65 @@
+<?php  // vim: ts=4 sts=4 sw=4
+require_once 'PHPUnit/Framework.php';
+
+set_include_path('./lib');
+require_once 'Skinny/Profiler.php';
+
+class TestSkinnyProfiler extends PHPUnit_Framework_TestCase
+{
+    private $class;
+
+    function setUp ( )
+    {
+        $this->class = new SkinnyProfiler( );
+    }
+
+    function testRecordQuery ( )
+    {
+        $profiler = $this->class;
+
+        $query = 'SELECT * FROM user';
+        $expect[ ] = $query;
+        $profiler->record_query($query);
+        $this->assertEquals($expect, $profiler->query_log);
+
+        $query = '
+            SELECT
+                id, name
+            FROM
+                user
+            WHERE
+                name like "%neko%"
+        ';
+        $expect[ ] = 'SELECT id, name FROM user WHERE name like "%neko%"';
+        $profiler->record_query($query);
+        $this->assertEquals($expect, $profiler->query_log);
+
+        $profiler->reset( );
+        $expect = array( );
+        $this->assertEquals($expect, $profiler->query_log);
+    }
+
+    function testBindValues ( )
+    {
+        $profiler = $this->class;
+
+        $query = 'SELECT id FROM user WHERE id = ?';
+        $bind  = array(1);
+        $expect[ ] = $query.' :binds 1';
+        $profiler->record_query($query, $bind);
+        $this->assertEquals($expect, $profiler->query_log);
+
+        $query = 'SELECT id FROM user WHERE (id = ? OR id = ?)';
+        $bind  = array(1, 2);
+        $expect[ ] = $query.' :binds 1, 2';
+        $profiler->record_query($query, $bind);
+
+        $query = '
+            INSERT INTO user (name) VALUES (?)
+        ';
+        $expect[ ] = 'INSERT INTO user (name) VALUES (?) :binds null';
+        $profiler->record_query($query, array(null));
+
+        $this->assertEquals($expect, $profiler->query_log);
+    }
+}
